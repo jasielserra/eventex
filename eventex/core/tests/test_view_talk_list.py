0878a47405ede3203aec92a4c53255a4aa07236f
Jasiel Serra <jasiel_serra@hotmail.com>
2016-04-05 15:45:30 -0300

    Course como Abstract Base Class

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
-from eventex.core.models import Talk, Speaker
+from eventex.core.models import Talk, Speaker, Course
 
 
 class TalkListGet(TestCase):
@@ -10,11 +10,16 @@ class TalkListGet(TestCase):
         t2 = Talk.objects.create(title='Título da Palestra', start='13:00',
                             description='Descrição da Palestra')
 
+        c1 = Course.objects.create(title='Título do Curso', start='09:00',
+                            description='Descrição do Curso', slots=20)
+
+
         speaker = Speaker.objects.create(name = 'Jasiel Serra',
                                          slug = 'jasiel-serra',
                                          website = 'http://jasielserra.com')
         t1.speakers.add(speaker)
         t2.speakers.add(speaker)
+        c1.speakers.add(speaker)
 
         self.resp = self.client.get(r('talk_list'))
 
@@ -29,9 +34,12 @@ class TalkListGet(TestCase):
             (2,'Título da Palestra'),
             (1,'10:00'),
             (1,'13:00'),
-            (2,'/palestrantes/jasiel-serra/'),
-            (2,'Jasiel Serra'),
+            (3,'/palestrantes/jasiel-serra/'),
+            (3,'Jasiel Serra'),
             (2,'Descrição da Palestra'),
+            (1,'Título do Curso'),
+            (1, '09:00'),
+            (1,'Descrição do Curso'),
         ]
 
         for count, expected in contents:
@@ -39,7 +47,7 @@ class TalkListGet(TestCase):
                 self.assertContains(self.resp, expected, count)
 
     def test_context(self):
-        variables = ['afternoon_talks', 'morning_talks']
+        variables = ['afternoon_talks', 'morning_talks', 'courses']
 
         for key in variables:
             with self.subTest():
